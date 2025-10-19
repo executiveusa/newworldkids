9b9752fb03eced222b60d75e3b2bdeeab91982f1
executiveusa <executiveusa@gmail.com>
2025-10-19 06:33:04 +0000

    feat: Implement Trail Mixx radio platform

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,26 +1,17 @@
 { pkgs, ... }: {
-
-  # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
-
-  # Use https://search.nixos.org/packages to find packages
+  channel = "stable-23.11";
   packages = [
     pkgs.nodejs_22***REMOVED***,
     pkgs.yarn,
-    pkgs.turbo
+    pkgs.turbo,
+    pkgs.openjdk
   ];
-
-  # Sets environment variables in the workspace
   env = {
     SOME_ENV_VAR = "hello";
   };
-
-  # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
   idx.extensions = [
     "angular.ng-template"
   ];
-
-  # Enable previews and customize configuration
   idx.previews = {
     enable = true;
     previews = {
